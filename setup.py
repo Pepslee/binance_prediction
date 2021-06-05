a85e4b86e3fc65870cf73b01bfcb6fdecdbe4d67
denja <ikuzen@gmail.com>
2021-06-05 11:59:42 +0300

    add: get_packages_from_pipfile_lock function

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 import glob
+import json
 
 from setuptools import setup, find_packages
 
 from binance_prediction import __version__
+
+
+def get_packages_from_pipfile_lock(path: str):
+    with open(path, "r") as read_file:
+        data = json.load(read_file)
+        packages_list = [f"{item[0]}{item[1]['version']}" for item in data['default'].items()]
+        packages = '\n'.join(packages_list)
+        return packages
+
 
 setup(
     name='binance_prediction',
@@ -11,8 +21,8 @@ setup(
     description='{package description}',
     long_description=open('README.md').read(),
     url='https://github.com/Pepslee/binance_prediction',
-    install_requires=open('requirements.txt').read(),
-    scripts=glob.glob('bin/[!_]*'),
+    install_requires=get_packages_from_pipfile_lock('Pipfile.lock'),
+    scripts=glob.glob('bin/*.py'),
     test_suite='tests',
     include_package_data=True
 )
